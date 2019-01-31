279dd9b30ac84d254e5e777d8c186f473f0a9722
Alan Hu <on.alanhu@gmail.com>
2019-01-31 20:18:14 +0100

    solution

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 // INPUT IS A STRING, RETURNS A NUMBER
 
 function lowestProduct(input) {
-
-
+  let arr = input.split('')
+  if (arr.length < 4) return 'Number is too small';
+  arr = arr.map(str => Number(str));
+  const res = [];
+  for (let i = 0; i <= arr.length -4; i++) {
+    res.push(arr[i] * arr[i + 1] * arr[i + 2] * arr[i + 3]);
+  }
+  return Math.min(...res);
 }
-
+lowestProduct('9123456789');
 module.exports = lowestProduct;
