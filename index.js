f614e60994e49617c147960c9892c49930648dd6
Rodrigo Alcala <rodri.a90@gmail.com>
2019-01-31 20:19:21 +0100

    🎉

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 // INPUT IS A STRING, RETURNS A NUMBER
 
 function lowestProduct(input) {
-
-
+  if (input.length < 4) return 'Number is too small';
+  let arr = input.split('');
+  let minMult = parseInt(arr[0]) * parseInt(arr[1]) * parseInt(arr[2]) * parseInt(arr[3]);
+  for (let i = 1; i < arr.length - 4; i++) {
+    if (parseInt(arr[i]) * parseInt(arr[i+1]) * parseInt(arr[i+2]) * parseInt(arr[i+3]) < minMult) {
+      minMult = parseInt(arr[i]) * parseInt(arr[i+1]) * parseInt(arr[i+2]) * parseInt(arr[i+3]);
+    }
+  }
+  console.log(minMult);
+  return minMult;
 }
 
 module.exports = lowestProduct;
