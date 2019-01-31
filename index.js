23db7a6ced871f940c1e51be135e84250ec81c1a
Felix Weinberger <felix.weinberger@gmail.com>
2019-01-31 20:13:02 +0100

    solution

--- index.js
+++ index.js
@@ -2,6 +2,21 @@
 
 function lowestProduct(input) {
 
+  let products = [];
+
+  if (input.length < 4) {
+    return 'Number is too small'
+  } else {
+    for (let i = 0; i <= input.length - 4; i++) {
+      let product = Number(input[i]) * 
+        Number(input[i+1]) * 
+        Number(input[i+2]) * 
+        Number(input[i+3]);
+      products.push(product);
+    }
+  }
+  
+  return Math.min(...products);
 
 }
 
